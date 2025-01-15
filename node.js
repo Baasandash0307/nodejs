acf3ea48876a3e3df4fe4a9b1bb921d7b653c281
Baasandash0307 <bsskaa.kai123@gmail.com>
2025-01-15 11:07:41 +0800

    Add files via upload

--- node.js
+++ node.js
@@ -1,20 +1,84 @@
 
 
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.writeFile('newFile.txt', 'Hello world!', function (err) {
-    if (err);
-    console.log('Saved!');
-  });
+// fs.writeFile('newFile.txt', 'Hello world!', function (err) {
+//     if (err);
+//         console.log('Saved!');
+//   });
 
-  fs.readFile('newFile.txt', 'utf8', (error, data) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    console.log('File content:', data);
-});
+// fs.readFile('newFile.txt', 'utf8', (error, data) => {
+//     if (error) {
+//         console.error(error);
+//         return;
+//     }
+//     console.log('File content:', data);
+// });
+
+
+
+
+
+    //  CREATE OPERATION
+// 1. read data from file
+// 2. update data
+// 3. write data on the file
+
+import fs from 'fs';
+function createStudent(student) {
+    // 1
+    const json = fs.readFileSync('example.json', 'utf-8')
+    const data = JSON.parse(json);
+
+    // 2 
+    data.push(student);
+
+    // 3 
+    fs.writeFileSync('example.json', JSON.stringify(data))
+}
+createStudent({name : "bat", seatNumber : "2A"})
+
+
+    //delete operation
+// 1. read data from file
+// 2. delete student from data
+// 3. write data to the file
+
+function deleteStudent(seatNumber){
+
+    // 1
+    const json = fs.readFileSync('example.json', 'utf-8');
+    const data = JSON.parse(json);
+
+    // 2 
+    const filteredData = data.filter(function(student){
+        return student.seatNumber != seatNumber
+    });
+
+    // 3
+    fs.writeFileSync('example.json', JSON.stringify(filteredData))
+
+}
+deleteStudent("1A")
+
+
+
+
+
+
+
+
+
+
 
 
   
+
+
+
+
+
+
+
+
 
